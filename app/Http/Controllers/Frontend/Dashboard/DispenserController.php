e9fb7c6895315ebe2ab403ebc5e01d26e5198fe8
salmanabdurrahman <salmanabdurrahman12345@gmail.com>
2025-04-04 14:22:21 +0700

    feat: initialize admin and patient controllers

---
+++ app/Http/Controllers/Frontend/Dashboard/DispenserController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Frontend\Dashboard;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class DispenserController extends Controller
+{
+    //
+}
